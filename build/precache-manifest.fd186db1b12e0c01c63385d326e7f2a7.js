de6dff5bceff34023d42f4713f8df11c403a396b
24622 <jiaojiao.chen01@hand-china.com>
2019-07-29 18:43:10 +0800

    :this is my react app

---
+++ build/precache-manifest.fd186db1b12e0c01c63385d326e7f2a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6602240eed388f17a949a3fcaa97b057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddda8670f0e663c7b3f1",
+    "url": "/static/css/main.8fdd7cc4.chunk.css"
+  },
+  {
+    "revision": "804dddfdf5dbe586ae57",
+    "url": "/static/js/2.d9c3707f.chunk.js"
+  },
+  {
+    "revision": "ddda8670f0e663c7b3f1",
+    "url": "/static/js/main.00a4eeb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1417afd7076b7eb9f26dbbe6599127ec",
+    "url": "/static/media/img1.1417afd7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
